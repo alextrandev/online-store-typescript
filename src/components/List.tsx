5107d1886656bf599829f2cf12f688beff57eff9
Alex Tran <tranduclong092@gmail.com>
2024-09-27 13:43:11 +0300

    Feat: add useeffect block to fetch for products

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,9 +1,16 @@
-import { useAppSelector } from "../hooks/hooks"
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks"
 import Product from './Product';
-
+import { fetchProducts } from "../features/productSlice";
 
 export default function List() {
   const products = useAppSelector((state) => state.products.products);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
   return (
     <div>
       {products.map((product) =>
